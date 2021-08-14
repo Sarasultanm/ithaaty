7c19d5e3bb89d130cf04096d01b92af71752bccf
EduardoB <ocojcastro@gmail.com>
2021-08-15 00:46:21 +0800

    Update Logo size
    Update logo size

--- resources/views/layouts/editor/header.blade.php
+++ resources/views/layouts/editor/header.blade.php
@@ -4,7 +4,7 @@
         <div class="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
           <div class="flex-shrink-0 flex items-center">
             <a href="#">
-              <img class="block h-8 w-auto" src="{{ asset('images/logo.png') }}" alt="Workflow">
+              <img class="block  w-auto" src="{{ asset('images/logo.png') }}" alt="Workflow" style="height:70px;">
             </a>
           </div>
         </div>
