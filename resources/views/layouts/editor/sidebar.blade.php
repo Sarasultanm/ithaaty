a1e2357d127f86d34a8b85c4f4171e57f334b3cf
EduardoB <ocojcastro@gmail.com>
2021-08-27 11:39:27 +0800

    Change overview to analytics
    Change overview to analytics

--- resources/views/layouts/editor/sidebar.blade.php
+++ resources/views/layouts/editor/sidebar.blade.php
@@ -28,7 +28,7 @@
             <x-nav-link-custom :href="route('editorOverview')" :active="request()->routeIs('editorOverview')">
               <!-- Heroicon name: outline/fire -->
               <svg xmlns="http://www.w3.org/2000/svg" class="text-gray-400 group-hover:text-gray-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg><span class="truncate">Overview</span>
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" /></svg><span class="truncate">Analytics</span>
              </x-nav-link-custom>
 
              <x-nav-link-custom :href="route('editorNotes')" :active="request()->routeIs('editorNotes')">
