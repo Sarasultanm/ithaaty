0ff5c36c5bdb491bd69c0c2c06cb5e829803e04e
orlandcastro <ocojcastro@gmail.com>
2022-06-21 14:29:55 +0800

    Update aside audio
    Update aside audio

--- resources/views/layouts/editor/aside.blade.php
+++ resources/views/layouts/editor/aside.blade.php
@@ -217,43 +217,41 @@
                     <div class="flow-root mt-6">
                       <ul class="-my-4 divide-y divide-gray-200">
                         @foreach ($recommended as $reco )
-                        <li class="relative flex py-4 space-x-3">
-                          <div class="flex-shrink-0">
-                            {{-- @if ($reco->get_audio->get_thumbnail->count() != 0) 
-                                @if(empty($reco->get_audio->get_thumbnail->first()->gallery_path))
+                          @if($reco->get_audio->count() != 0)
+                            <li class="relative flex py-4 space-x-3">
+                              <div class="flex-shrink-0">
+                                @if ($reco->get_audio->get_thumbnail->count() != 0) 
+                                    @if(empty($reco->get_audio->get_thumbnail->first()->gallery_path))
+                                        @php 
+                                            $reco_s3_thumbnail = asset('images/default_podcast.jpg');
+                                        @endphp 
+                                    @else
+                                      @php 
+                                          $reco_img_path =$reco->get_audio->get_thumbnail->first()->gallery_path; 
+                                          $reco_s3_thumbnail = config('app.s3_public_link')."/users/podcast_img/".$episode_img_path; 
+                                      @endphp 
+
+                                    @endif
+                                @else 
                                     @php 
                                         $reco_s3_thumbnail = asset('images/default_podcast.jpg');
                                     @endphp 
-                                @else
-                                  @php 
-                                      $reco_img_path =$reco->get_audio->get_thumbnail->first()->gallery_path; 
-                                      $reco_s3_thumbnail = config('app.s3_public_link')."/users/podcast_img/".$episode_img_path; 
-                                  @endphp 
-
                                 @endif
-                            @else 
-                                @php 
-                                    $reco_s3_thumbnail = asset('images/default_podcast.jpg');
-                                @endphp 
-                            @endif --}}
 
-                            @php 
-                                $reco_s3_thumbnail = asset('images/default_podcast.jpg');
-                            @endphp 
-
-                            <img class="w-12 h-12 rounded" src="{{ $reco_s3_thumbnail }}">
-                          </div>
-                          <div class="flex-1 min-w-0">
-                            <p class="text-sm font-bold text-gray-800">{{ $reco->get_audio->audio_name }}</p>
-                            <p class="text-sm text-gray-800"> {{ Str::limit( $reco->get_audio->audio_summary , 30) }}
-                            </p>
-                        
-                          </div>
-                          <a 
-                          href="{{ route('editorPodcastView',['id' => $reco->get_audio->id]) }}"
-                            class="absolute inset-0 cursor-pointer" 
-                            aria-hidden="true"></a>
-                        </li>
+                                <img class="w-12 h-12 rounded" src="{{ $reco_s3_thumbnail }}">
+                              </div>
+                              <div class="flex-1 min-w-0">
+                                <p class="text-sm font-bold text-gray-800">{{ $reco->get_audio->audio_name }}</p>
+                                <p class="text-sm text-gray-800"> {{ Str::limit( $reco->get_audio->audio_summary , 30) }}
+                                </p>
+                            
+                              </div>
+                              <a 
+                              href="{{ route('editorPodcastView',['id' => $reco->get_audio->id]) }}"
+                                class="absolute inset-0 cursor-pointer" 
+                                aria-hidden="true"></a>
+                            </li>
+                          @endif
                         @endforeach
                         <!-- More posts... -->
                       </ul>
